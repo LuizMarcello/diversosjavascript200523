338b843c318027a6ed7662161694e7fc66a7b568
luizmarcello <luizmarcello.vmo@hotmail.com>
2023-06-21 22:31:11 -0300

    Londrina:21.06.23_22:31hs

---
+++ functions/js/scripts.js
@@ -0,0 +1,20 @@
+/* ESTRUTURA INICIAL */
+
+/* Assim é "declaração literal" */
+/* Quando chama a função, o nome é "argumento" */
+/* Quando declara uma função, o nome é "parâmetro" */
+function nomeDaFuncao() {
+  //Código aqui
+
+  //No final retorna explicitamente ou implicitamente
+}
+
+let variavel = 'Esta string será substituida pelo return da próxima função'
+
+function funcReturnUndefined() {
+  let num1 = 10,
+    num2 = 10,
+    soma = num1 + num2
+}
+
+variavel = funcReturnUndefined()
